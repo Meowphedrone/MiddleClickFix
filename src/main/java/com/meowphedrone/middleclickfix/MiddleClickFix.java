54d3a21b17c90096d77c732db4444c3c04416c43
Meowphedrone <128436177+Meowphedrone@users.noreply.github.com>
2024-08-10 00:57:31 +0400

    Add files via upload

---
+++ src/main/java/com/meowphedrone/middleclickfix/MiddleClickFix.java
@@ -0,0 +1,6 @@
+package com.meowphedrone.middleclickfix;
+import net.fabricmc.api.ModInitializer;
+public class MiddleClickFix implements ModInitializer {
+    @Override
+    public void onInitialize() {}
+}
---
+++ src/main/java/com/meowphedrone/middleclickfix/client/MiddleClickFixClient.java
@@ -0,0 +1,6 @@
+package com.meowphedrone.middleclickfix.client;
+import net.fabricmc.api.ClientModInitializer;
+public class MiddleClickFixClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
